226d87194d5f7db7d78d76f7c83a53d7da2ce06d
tony <56626062+loveleaves@users.noreply.github.com>
2022-06-17 16:59:38 +0800

    fix(vqa_large_best.sh)
    Fix google cloud download problem

--- vqa_large_best.sh
+++ vqa_large_best.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-2KSdpfikTj9fnXjluuMOeFpguBrNyM2' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-2KSdpfikTj9fnXjluuMOeFpguBrNyM2" -O 'vqa_large_best.pt' && rm -rf /tmp/cookies.txt
+#wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-2KSdpfikTj9fnXjluuMOeFpguBrNyM2' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-2KSdpfikTj9fnXjluuMOeFpguBrNyM2" -O 'vqa_large_best.pt' && rm -rf /tmp/cookies.txt
+pip install gdown
+gdown --id '1-2KSdpfikTj9fnXjluuMOeFpguBrNyM2' --output vqa_large_best.pt
